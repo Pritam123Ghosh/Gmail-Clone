741eeb948fcf49489d02e82cee4150a88d913429
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-06-11 13:02:24 +0530

    update url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8000";
+const API_URL = "https://gmail-clone-api.vercel.app/";
 const API_GMAIL = async (urlObject, payload, type) => {
     return await axios({
         method: urlObject.method,
